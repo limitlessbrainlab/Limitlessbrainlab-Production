b410cf13642b0ed0aea91d818c34f7c306d697ef
Murali32 <chatgptnotes@gmail.com>
2026-07-07 14:35:22 +0530

    Use Gmail SSL SMTP on Render

--- Neuro360/server/index.js
+++ Neuro360/server/index.js
@@ -60,8 +60,8 @@ const activeTransporter = (() => {
   if (gmailConfigured) {
     return nodemailer.createTransport({
       host: 'smtp.gmail.com',
-      port: 587,
-      secure: false,
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
